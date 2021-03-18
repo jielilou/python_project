e7996dc11324201ae18325bcace2246e0d6e8dcc
jielilou <877288704@qq.com>
2021-03-18 23:16:23 +0800

    add calcularor.py

---
+++ calculator.py
@@ -0,0 +1,21 @@
+# 加法
+def function_add(value1, value2):
+    return value1 + value2
+
+# 减法
+def function_sub(value1, value2):
+    return value1 - value2
+
+# 乘法
+def function_mul(value1, value2):
+    return value1*value2
+
+# 除法
+def function_div(value1, value2):
+    return value1 / value2
+
+if __name__ == "__main__":
+    print(function_add(1, 2))
+    print(function_sub(1, 2))
+    print(function_mul(1, 2))
+    print(function_div(1, 2))
